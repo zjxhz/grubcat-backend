cbda9a2f55c7a356fcec99aff2098753b66347a3
qimeng <qimeng@JHCMWM1.nsn-intra.net>
2012-05-30 12:57:54 +0800

    Merge

--- grubcat/settings.py
+++ grubcat/settings.py
@@ -129,6 +129,17 @@ SERIALIZATION_MODULES = {
     'json': 'wadofstuff.django.serializers.json'
 }
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.core.context_processors.request",
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages"
+    )
+
 from easy_thumbnails import defaults
 
 THUMBNAIL_PROCESSORS = (
