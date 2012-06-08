656978cd7e708ec6a18b345107f9e5f5fcde3f35
peter <peter@localhost>
2012-06-09 03:58:38 +0800

    fix ie6 style issues

--- grubcat/eo/assets.py
+++ grubcat/eo/assets.py
@@ -12,6 +12,11 @@ meal_detail_css=Bundle(
     'css/base.css','css/meal-detail.css', filters='cssmin', output='gen/meal-detail.%(version)s.css'
 )
 
+dd_fixPNG_js=Bundle(
+    'js/DD_belatedPNG_0.0.8a.js', filters='jsmin', output='gen/DD_belatedPNG_0.0.8a.%(version)s.js'
+)
+
 register('base_css', base_css)
 register('meal_list_css', meal_list_css)
 register('meal_detail_css', meal_detail_css)
+register('dd_fixPNG_js', dd_fixPNG_js)
