87272d5c5e6125ff0c8769d1d3be44151b268f9e
peter <peter@localhost>
2012-06-10 16:57:45 +0800

    modify static root

--- grubcat/settings.py
+++ grubcat/settings.py
@@ -70,7 +70,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/fanju/webapps/static'
+STATIC_ROOT = '/home/fanju/webapps/static/'
 
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.CachedStaticFilesStorage"
 
