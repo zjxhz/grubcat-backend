f0826524f155c294b00d4ff3a8879021d2edea97
peter <peter@localhost>
2012-06-18 19:39:27 +0800

    Merge

--- grubcat/settings_localhost.py
+++ grubcat/settings_localhost.py
@@ -2,14 +2,7 @@
 import os
 from settings import *
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'easyorder',                      # Or path to database file if using sqlite3.
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '',
-        'OPTIONS': {"init_command": "SET storage_engine=INNODB",}
-    }
-}
+ASSETS_AUTO_BUILD = True
+DEBUG=True
+
+
