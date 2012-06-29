a06ab9b28201e4f2b8e427a4d780686afacda9b1
peter <peter@localhost>
2012-06-29 09:39:46 +0800

    gen_code script

--- grubcat/eo/static/js/restaurant-admin.js
+++ grubcat/eo/static/js/restaurant-admin.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
                 })
             }
         });
-        event.preventDefault();
+        return false;
     });
 
     // bind form using 'ajaxForm'
