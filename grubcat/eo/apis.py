4a4782c737e538d3bb68e2bc51db122a413477c9
zjxhz <zjxhz@localhost>
2012-07-30 15:00:52 +0800

    user/{user_id}/recommendations/ returns users with similar tags

--- grubcat/eo/apis.py
+++ grubcat/eo/apis.py
@@ -96,9 +96,11 @@ def get_my_list(resource, queryset, request):
     to_be_serialized = resource.alter_list_data_to_serialize(request, to_be_serialized)
     return resource.create_response(request, to_be_serialized)
 
+#todo maybe we can use decorator
 def login_required(request):
     response = {"status": "NOK", "info": "You were not logged in"}
     return HttpResponse(simplejson.dumps(response))
+         
 
 # Create a general response with status and message)
 def createGeneralResponse(status, message, extra_dict=None):
@@ -195,6 +197,8 @@ class UserResource(ModelResource):
                 self.wrap_view('get_feeds'), name="api_get_feeds"),
             url(r"^(?P<resource_name>%s)/(?P<pk>\d+)/tags%s$" % (self._meta.resource_name, trailing_slash()),
                 self.wrap_view('get_tags'), name="api_get_tags"),    
+            url(r"^(?P<resource_name>%s)/(?P<pk>\d+)/recommendations%s$" % (self._meta.resource_name, trailing_slash()),
+                self.wrap_view('get_recommendations'), name="api_get_recommendations"),   
         ]
     
     
@@ -312,8 +316,17 @@ class UserResource(ModelResource):
                 raise
         else:
             return get_my_list(UserTagResource(), user.tags.all(), request) 
+
+    def get_recommendations(self, request, **kwargs):
+        if not request.user.is_authenticated():
+            return login_required(request)
+        user = self.cached_obj_get(request=request, **self.remove_api_resource_names(kwargs))        
+        if request.method == 'GET':
+            recommended_users = user.tags.similar_objects()
+            return get_my_list(UserResource(), recommended_users, request)
+        else:
             raise
-        
+            
     class Meta:
         queryset = UserProfile.objects.all()
         resource_name = 'user'
