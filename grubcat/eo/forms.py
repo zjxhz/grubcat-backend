de0310ca37d33725295de419e1c7b9fbf08954a4
qimeng <qimeng@JHCMWM1.nsn-intra.net>
2012-08-13 14:27:37 +0800

    create a meal,complete

--- grubcat/eo/forms.py
+++ grubcat/eo/forms.py
@@ -1,20 +1,39 @@
 #coding=utf-8
 from django import forms
 from django.forms import ModelForm
+from django.forms.util import to_current_timezone
 from django.forms.widgets import *
 from grubcat.eo.models import *
 
+SplitDateTimeWidget
+
 
 class MealForm(ModelForm):
-    time = forms.SplitDateTimeField(input_date_formats='yyyy-MM-dd',input_time_formats='HH:mm')
-    menu_id = forms.CharField(widget=HiddenInput)
+    menu_id = forms.CharField(widget=HiddenInput,required=False)
+    if_let_fanjoin_choose = forms.BooleanField(widget=HiddenInput,initial=False)
     class Meta:
         model = Meal
-        fields = ('topic','introduction','privacy','time','region','min_persons','list_price','extra_requests')
+        fields = ('topic','introduction','privacy','start_date','start_time','region','min_persons','list_price','extra_requests','if_let_fanjoin_choose')
         widgets = {
             'introduction': Textarea({'rows':5}),
             'extra_requests':Textarea({'rows':5}),
         }
+
+    def clean(self):
+        cleaned_data = super(MealForm, self).clean()
+        menu_id = cleaned_data.get("menu_id")
+        list_price = cleaned_data.get("list_price")
+        region = cleaned_data.get("region")
+        if_let_fanjoin_choose = cleaned_data.get("if_let_fanjoin_choose")
+        if if_let_fanjoin_choose:
+            if not list_price:
+                self._errors["list_price"] = self.error_class([u'请选择平均消费'])
+            if not region:
+                self._errors["region"] = self.error_class([u'请选择区域'])
+        elif not menu_id:
+            self._errors["menu_id"] = self.error_class([u'请您在下面选择一个套餐或者点击"让饭聚网帮我选"按钮'])
+        return cleaned_data
+
 
 
 NUM_PERSON_CHOICE = [(x, "%s人" % x) for x in range(1, 10)]
@@ -53,6 +72,9 @@ class MenuForm(ModelForm):
     class Meta:
         model = Menu
         fields  = ('num_persons','average_price') # TODO 'photo'
+        widgets={
+            'average_price':Select(attrs={'class':'input-small'})
+        }
 
 
 class UploadFileForm(forms.Form):
