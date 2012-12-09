1285d10f49b4228aabe00250516510ec1240b73a
Huanze Xu <xuhuanze@qq.com>
2012-12-09 16:33:25 +0800

    change back to use one-off reactor...

--- grubcat/eo/xmpp.py
+++ grubcat/eo/xmpp.py
@@ -1,15 +1,26 @@
+from django.conf import settings
+from twisted.application import service
 from twisted.internet import reactor
+from twisted.python import log
 from twisted.words.protocols.jabber.jid import JID
-from wokkel.client import XMPPClient
+from wokkel import client
+from wokkel.client import XMPPClient, XMPPClientConnector
 from wokkel.compat import IQ
 from wokkel.subprotocols import XMPPHandler
 from wokkel.xmppim import Presence
 import hashlib
 import logging
-from django.conf import settings
-from models import UserProfile
 
 logger = logging.getLogger('api')
+
+def clientCreator(factory, host=None):
+    if not host:
+        domain = factory.authenticator.jid.host
+    else:
+        domain = host
+    c = XMPPClientConnector(reactor, domain, factory)
+    c.connect()
+    return factory.deferred
 
 class AvatarUpdatedPresence(Presence):
     def __init__(self, sender, sha1):
@@ -21,78 +32,67 @@ class AvatarUpdatedPresence(Presence):
 NS_AVATAR='http://jabber.org/protocol/pubsub'
 NS_VCARD_AVATAR='vcard-temp'
 NS_ROSTER_ITEM='jabber:iq:roster'
+        
+def addOrUpdateRosterItem(jid, name, xmlstream):       
+    iq = IQ(xmlstream, 'set')
+    iq.addElement((NS_ROSTER_ITEM, 'query'))
+    item = iq.query.addElement('item')
+    item['jid'] = jid
+    item['name'] = name
+    item['subscription']='both'
+    d = iq.send()
+    return d
 
-class RosterClient(XMPPHandler):
-    
-    def __init__(self, jid, name):
-        self.jid = jid
-        self.name = name
         
-    def addOrUpdateRosterItem(self):
-        def processRosterResult(result):
-            print result.toXml()
-            
-        iq = IQ(self.xmlstream, 'set')
-        iq.addElement((NS_ROSTER_ITEM, 'query'))
-        item = iq.query.addElement('item')
-        item['jid'] = self.jid
-        item['name'] = self.name
-        item['subscription']='both'
-        d = iq.send()
-        d.addCallback(processRosterResult)
-        return d
-    
-    def connectionInitialized(self):
-        self.addOrUpdateRosterItem()
+def publishvCardAvatar(jid, avatar, xmlstream):
+    def processvCardAvatarUpdateResult(result):
+        avatarUpdatedNotif = AvatarUpdatedPresence(jid, avatar_sha1)
+        xmlstream.send(avatarUpdatedNotif.toXml())
         
-class AvatarClient(XMPPHandler):
-    def __init__(self, jid, avatar):
-        self.jid = jid
-        self.avatar = avatar
-        
-    def connectionInitialized(self):
-        self.publishvCardAvatar()
-
-    def publishvCardAvatar(self):
-        def processvCardAvatarUpdateResult(result):
-            avatarUpdatedNotif = AvatarUpdatedPresence(self.jid, self.avatar_sha1)
-            self.send(avatarUpdatedNotif)
-            
-        iq = IQ(self.xmlstream, 'set')
-        iq['from']=self.jid
-        vCard = iq.addElement((NS_VCARD_AVATAR, 'vCard'))
-        photo = vCard.addElement('PHOTO')
-        photo.addElement('TYPE', content='image/jpeg')
-        content = open(self.avatar, "rb").read().encode("base64")
-        self.avatar_sha1 = hashlib.sha1(content).digest()
-        photo.addElement('BINVAL', content=content)
-        d = iq.send()
-        d.addCallback(processvCardAvatarUpdateResult)
-        return d
+    iq = IQ(xmlstream, 'set')
+    iq['from']=jid
+    vCard = iq.addElement((NS_VCARD_AVATAR, 'vCard'))
+    photo = vCard.addElement('PHOTO')
+    photo.addElement('TYPE', content='image/jpeg')
+    content = open(avatar, "rb").read().encode("base64")
+    avatar_sha1 = hashlib.sha1(content).hexdigest()
+    photo.addElement('BINVAL', content=content)
+    d = iq.send()
+    d.addCallback(processvCardAvatarUpdateResult)
+    return d
 
 def syncName(username, password, name):
     logger.debug("sync name of user '%s' to %s" % (username, name))
     jidStr = username + "@fanjoin.com"
-    client = XMPPClient(JID(jidStr), password, settings.XMPP_SERVER) 
-    roster = RosterClient(jidStr, name)
-    roster.setHandlerParent(client)
-    client.startService()
+    jid = JID(jidStr)
+    factory = client.DeferredClientFactory(jid, password)    
+    factory.streamManager.logTraffic = True
+    d = clientCreator(factory,"42.121.34.164")
+    d.addCallback(lambda _ : addOrUpdateRosterItem(jidStr, "Wayne1234", factory.streamManager.xmlstream) )
+    d.addBoth(lambda _: reactor.callLater(1, reactor.stop))
+    d.addErrback(log.err)
+    reactor.run()
+
 
 def syncAvatar(username, password, avatar):
     logger.debug("sync avatar of user '%s'" % username)
     jidStr = username + "@fanjoin.com"
-    client = XMPPClient(JID(jidStr), password, settings.XMPP_SERVER) 
-    avatar = AvatarClient(jidStr, avatar)
-    avatar.setHandlerParent(client)
-    client.startService()
-  
+    jid = JID(jidStr)
+    factory = client.DeferredClientFactory(jid, password)    
+    d = clientCreator(factory, "42.121.34.164")
+    d.addCallback(lambda _ : publishvCardAvatar(jidStr, avatar, factory.streamManager.xmlstream) )
+    d.addErrback(log.err)
+    d.addBoth(lambda _: reactor.callLater(1, reactor.stop))
+    reactor.run()
 
 def syncBoth(user_profile):
     syncName(user_profile.user.username, user_profile.user.password, user_profile.name)
     if user_profile.avatar:
         syncAvatar(user_profile.user.username, user_profile.user.password, user_profile.small_avatar_path)
 
-reactor.run()    
+#syncName("xuaxu","qqqqqq","Waynexyz")
+#syncAvatar("xuaxu","qqqqqq","/home/fanju/media/uploaded_images/2012/09/11/abc.com_avatar.jpg.50x50_q85_crop_detail.jpg")
+#reactor.run()    
 #syncName("xuaxu","pbkdf2_sha256$10000$SOpptq1FcF8k$c8ttyX5qWC+bLlC71E2wPoFB54+oOz4wsleOKLptNBU=", "Wayne")
 #syncAvatar("xuaxu","pbkdf2_sha256$10000$SOpptq1FcF8k$c8ttyX5qWC+bLlC71E2wPoFB54+oOz4wsleOKLptNBU=", "/home/fanju/media/uploaded_images/2012/11/06/file_1.50x50_q85_crop_detail.jpg")
 #    def publishAvatar(self):
