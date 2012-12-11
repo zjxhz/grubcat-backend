da7a575a39525417a72ec38807e10621420974a3
Huanze Xu <xuhuanze@qq.com>
2012-12-11 21:14:33 +0800

    added some logs

--- grubcat/eo/util.py
+++ grubcat/eo/util.py
@@ -1,6 +1,10 @@
 import pyapns
 import socket
 import json 
+import logging
+
+logger = logging.getLogger("api")
+
 class PyapnsWrapper(object):
  
     def __init__(self, host, app_id, apns_certificate_file, mode='sandbox'):
@@ -17,13 +21,17 @@ class XMPPClientWrapper(object):
     PORT = 5055
     
     def syncName(self, user_profile):
+        logger.debug("sync name of %s " % user_profile)
         soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         soc.connect(('localhost',self.PORT))
         dic = {'username':user_profile.user.username, "password":user_profile.user.password, "name": user_profile.name}
         soc.send(json.dumps(dic))
+        logger.debug("sync name finished with response: %s " % soc.recv(1024))
     
     def syncAvatar(self, user_profile):
+        logger.debug("sync avatar of %s " % user_profile)
         soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         soc.connect(('localhost',self.PORT))
         dic = {'username':user_profile.user.username, "password":user_profile.user.password, "avatar": user_profile.small_avatar_path}
         soc.send(json.dumps(dic))
+        logger.debug("sync avatar finished with response: %s " % soc.recv(1024))
