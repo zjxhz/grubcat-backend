d668c81618d1fb95d8b1204cd8cc8c5885ddd1fb
Huanze Xu <xuhuanze@qq.com>
2012-12-11 20:52:52 +0800

    use a simple socket client to connect to a xmpp proxy, this seems to be how twisted app works

--- grubcat/eo/util.py
+++ grubcat/eo/util.py
@@ -1,5 +1,6 @@
 import pyapns
- 
+import socket
+import json 
 class PyapnsWrapper(object):
  
     def __init__(self, host, app_id, apns_certificate_file, mode='sandbox'):
@@ -11,3 +12,18 @@ class PyapnsWrapper(object):
  
     def notify(self, token, message):
         pyapns.notify(self.app_id,token,{'aps':{'alert': message}})
+
+class XMPPClientWrapper(object):
+    PORT = 5055
+    
+    def syncName(self, user_profile):
+        soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        soc.connect(('localhost',self.PORT))
+        dic = {'username':user_profile.user.username, "password":user_profile.user.password, "name": user_profile.name}
+        soc.send(json.dumps(dic))
+    
+    def syncAvatar(self, user_profile):
+        soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        soc.connect(('localhost',self.PORT))
+        dic = {'username':user_profile.user.username, "password":user_profile.user.password, "avatar": user_profile.small_avatar_path}
+        soc.send(json.dumps(dic))
