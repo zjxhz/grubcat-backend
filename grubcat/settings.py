bbf91f9185423411ccd4370f904320291f839cf7
peter <peter@localhost>
2012-12-25 19:37:58 +0800

    fix bug -  cachedfilestorage produce reduant files

--- grubcat/settings.py
+++ grubcat/settings.py
@@ -59,7 +59,7 @@ MEDIA_URL = '/media/'
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = '/home/fanju/webapps/static/'
-
+ASSETS_ROOT=r'/home/fanju/src/grubcat-backend/grubcat/eo/static'
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.CachedStaticFilesStorage"
 
 # URL prefix for static files.
