a5f8567b4cb1cff5cfe6624c298fe9f9dcf11df9
peter <peter@localhost>
2013-01-04 15:14:27 +0800

    show small meal pics on meal list page

--- grubcat/eo/views.py
+++ grubcat/eo/views.py
@@ -643,8 +643,8 @@ def list_tags(request):
         tag_name_qs = Tag.objects.filter(name__icontains=query).values_list('name')
     else:
         tag_name_qs = UserProfile.tags.most_common().values_list('name','num_times' )
-        if len(tag_name_qs) < 20:
-            tag_name_qs=[(u'读书',),(u'学习',)]
+        if len(tag_name_qs) < 10:
+            tag_name_qs=[(u'读书',),(u'运动',),(u'唱歌',),(u'登山',),(u'骑行',),(u'公益',)]
     data = [{'value': tag[0]} for tag in tag_name_qs[:20]]
 
     return HttpResponse(simplejson.dumps(data), mimetype='application/json')
