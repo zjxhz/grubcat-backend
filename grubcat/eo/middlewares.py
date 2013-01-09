807e7979aa6b87952d771cd42b7e22d61e36cb6b
Huanze Xu <xuhuanze@qq.com>
2013-01-09 14:39:17 +0800

    close socket after sending

---
+++ grubcat/eo/middlewares.py
@@ -0,0 +1,73 @@
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core.files.base import ContentFile
+from eo.models import UserProfile, Gender
+import json
+import logging
+import urllib2
+import weibo
+
+logger = logging.getLogger("api")
+
+class WeiboAuthenticationBackend(object):
+    def authenticate(self, **credentials):
+        user_to_authenticate = None # tell Python explicitly this is a local variable
+        
+        access_token =credentials.get('access_token')
+        if not access_token:
+            raise
+        expires_in = credentials.get('expires_in')
+        if not expires_in:
+            expires_in = str(3600*24*14)
+        weibo_client = weibo.APIClient(app_key=settings.WEIBO_APP_KEY, app_secret=settings.WEIBO_APP_SECERT,redirect_uri=settings.WEIBO_REDIRECT_URL)
+        weibo_client.set_access_token(access_token, expires_in)
+        
+        if UserProfile.objects.filter(weibo_access_token=access_token).count():
+            user_profile = UserProfile.objects.get(weibo_access_token=access_token)
+            logger.debug("auth for %s OK" % user_profile)
+            user_to_authenticate = user_profile.user
+        else:
+            try:
+                logger.debug("access_token no match, a new weibo user or access_token has expired")
+                weibo_id = weibo_client.account.get_uid.get()["uid"]
+                if UserProfile.objects.filter(weibo_id=weibo_id).count():
+                    logger.debug("access_token found, update access token so next time it would be faster")
+                    user_profile = UserProfile.objects.get(weibo_id=weibo_id)
+                    user_profile.weibo_access_token=access_token
+                    user_profile.save()
+                    user_to_authenticate = user_profile.user
+                    return user_to_authenticate
+                else:
+                    logger.debug("Probably a new weibo user, create a new user for it")
+                    username,password='weibo_%s' % weibo_id, User.objects.make_random_password()
+                    user_to_authenticate = User.objects.create_user(username, '', password)
+                    user_to_authenticate.is_active = False #set unactive before complete some profile
+                    user_to_authenticate.save()
+                    user_profile = user_to_authenticate.get_profile()
+                    user_data = weibo_client.users.show.get(uid=weibo_id)
+                    user_profile.weibo_id = weibo_id
+                    user_profile.weibo_access_token = access_token
+                    user_profile.name = user_data.get('name')
+                    user_profile.motto = user_data.get('description') 
+                    if user_data.get('gender') == "m":
+                        user_profile.gender = Gender.MALE
+                    elif user_data.get('gender') == "f":
+                        user_profile.gender = Gender.FEMALE
+                    avatar_url = user_data.get('avatar_large') 
+                    user_profile.avatar.save(username+".jpg", ContentFile(urllib2.urlopen(avatar_url).read()), save=False)
+                    user_profile.save()
+                    return user_to_authenticate
+            except Exception as e:
+                if user_to_authenticate:
+                    user_to_authenticate.delete()
+                logger.exception("failed to auth %s " % user_to_authenticate)
+                raise e
+            
+        return user_to_authenticate
+        
+        
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return None
---
+++ grubcat/eo/xmpp.py
@@ -0,0 +1,112 @@
+from twisted.internet import reactor
+from twisted.python import log
+from twisted.words.protocols.jabber.jid import JID
+from wokkel import client
+from wokkel.client import  XMPPClientConnector
+from wokkel.compat import IQ
+from wokkel.xmppim import Presence
+import hashlib
+#import logging
+#
+#logger = logging.getLogger("api")
+
+def clientCreator(factory, host=None):
+    if not host:
+        domain = factory.authenticator.jid.host
+    else:
+        domain = host
+    c = XMPPClientConnector(reactor, domain, factory)
+    c.connect()
+    return factory.deferred
+
+class AvatarUpdatedPresence(Presence):
+    def __init__(self, sender, sha1):
+        Presence.__init__(self)
+        self['from']=sender
+        x = self.addElement(('vcard-temp:x:update', 'x'))
+        x.addElement('photo', content=sha1) 
+
+NS_AVATAR='http://jabber.org/protocol/pubsub'
+NS_VCARD_AVATAR='vcard-temp'
+NS_ROSTER_ITEM='jabber:iq:roster'
+        
+def addOrUpdateRosterItem(jid, name, xmlstream):       
+    iq = IQ(xmlstream, 'set')
+    iq.addElement((NS_ROSTER_ITEM, 'query'))
+    item = iq.query.addElement('item')
+    item['jid'] = jid
+    item['name'] = name
+    item['subscription']='both'
+    d = iq.send()
+    return d
+
+        
+def publishvCardAvatar(jid, avatar, xmlstream):
+    def processvCardAvatarUpdateResult(result):
+        avatarUpdatedNotif = AvatarUpdatedPresence(jid, avatar_sha1)
+        xmlstream.send(avatarUpdatedNotif.toXml())
+        
+    iq = IQ(xmlstream, 'set')
+    iq['from']=jid
+    vCard = iq.addElement((NS_VCARD_AVATAR, 'vCard'))
+    photo = vCard.addElement('PHOTO')
+    photo.addElement('TYPE', content='image/jpeg')
+    content = open(avatar, "rb").read().encode("base64")
+    avatar_sha1 = hashlib.sha1(content).hexdigest()
+    photo.addElement('BINVAL', content=content)
+    d = iq.send()
+    d.addCallback(processvCardAvatarUpdateResult)
+    return d
+
+def syncName(username, password, name):
+#    logger.debug("sync name of user '%s' to %s" % (username, name))
+    log.msg("sync name of user '%s' to %s" % (username, name))
+    jidStr = username + "@fanjoin.com"
+    jid = JID(jidStr)
+    factory = client.DeferredClientFactory(jid, password)    
+    factory.streamManager.logTraffic = True
+    d = clientCreator(factory,"42.121.34.164")
+    d.addCallback(lambda _ : addOrUpdateRosterItem(jidStr, name, factory.streamManager.xmlstream) )
+    d.addErrback(log.err)
+
+def syncAvatar(username, password, avatar):
+#    logger.debug("sync avatar of user '%s'" % username)
+    log.msg("sync avatar of user '%s'" % username)
+    jidStr = username + "@fanjoin.com"
+    jid = JID(jidStr)
+    factory = client.DeferredClientFactory(jid, password)    
+    d = clientCreator(factory, "42.121.34.164")
+    d.addCallback(lambda _ : publishvCardAvatar(jidStr, avatar, factory.streamManager.xmlstream) )
+    d.addErrback(log.err)
+
+
+def syncBoth(user_profile):
+    syncName(user_profile.user.username, user_profile.user.password, user_profile.name)
+    if user_profile.avatar:
+        syncAvatar(user_profile.user.username, user_profile.user.password, user_profile.small_avatar_path)
+
+syncName("xuaxu","qqqqqq","Waynexyz")
+#syncAvatar("xuaxu","qqqqqq","/home/fanju/media/uploaded_images/2012/09/11/abc.com_avatar.jpg.50x50_q85_crop_detail.jpg")
+
+
+#reactor.run()    
+#syncName("xuaxu","pbkdf2_sha256$10000$SOpptq1FcF8k$c8ttyX5qWC+bLlC71E2wPoFB54+oOz4wsleOKLptNBU=", "Wayne")
+#syncAvatar("xuaxu","pbkdf2_sha256$10000$SOpptq1FcF8k$c8ttyX5qWC+bLlC71E2wPoFB54+oOz4wsleOKLptNBU=", "/home/fanju/media/uploaded_images/2012/11/06/file_1.50x50_q85_crop_detail.jpg")
+#    def publishAvatar(self):
+#        def processResult(result):
+#            print type(result)
+#            print result.toXml()
+#            return result
+#            
+#        iq = IQ(self.xmlstream, 'set')
+#        iq['from']='xuaxu@fanjoin.com'
+#        pubsub = iq.addElement((NS_AVATAR, 'pubsub'))
+#        publish = pubsub.addElement('publish')
+#        publish['node'] = 'urn:xmpp:avatar:data'
+#        item = publish.addElement('item')
+#        item['id'] = '111f4b3c50d7b0df729d299bc6f8e9ef9066971f'
+#        item.addElement('data', 'urn:xmpp:avatar:data', content='qANQR1DBwU4DX7jmYZnncm')
+#        d = iq.send()
+#        d.addCallback(processResult)
+#        return d
+
