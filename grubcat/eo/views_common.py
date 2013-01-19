2d4e704913264c46d30518712f3e33363eabda6f
peter <peter@localhost>
2013-01-19 11:13:36 +0800

    add feature : follow/unfollow

--- grubcat/eo/views_common.py
+++ grubcat/eo/views_common.py
@@ -1,9 +1,37 @@
 #coding=utf-8
 from django.core.paginator import Paginator
 from django.http import HttpResponse
-import simplejson
+from django.utils import simplejson
 from taggit.models import Tag
 from eo.models import UserProfile
+
+SUCESS = "OK"
+ERROR = 'NOK'
+
+
+# Create a json response with status and message)
+def create_json_response(status=None, message=None, extra_dict=None):
+    response = {}
+    if status:
+        response['status'] = status
+    if message:
+        response['message'] = message
+    if extra_dict:
+        response.update(extra_dict)
+    return HttpResponse(simplejson.dumps(response), content_type='application/json', )
+
+
+def create_sucess_json_response(message=u"成功", extra_dict=None):
+    return create_json_response(SUCESS, message, extra_dict)
+
+
+def create_failure_json_response(message=u"操作失败", extra_dict=None):
+    return create_json_response(ERROR, message, extra_dict)
+
+
+def create_no_right_response(message=u'对不起，您没有权限执行此操作'):
+    return create_json_response(ERROR, message)
+
 
 def list_tags(request):
     query = request.GET.get('q', '')
@@ -20,4 +48,5 @@ def list_tags(request):
         data = [{'value': tag[0]} for tag in paginator.page(page).object_list]
     else:
         data = []
-    return HttpResponse(simplejson.dumps(data), mimetype='application/json')
+    return create_json_response(extra_dict=data)
+
