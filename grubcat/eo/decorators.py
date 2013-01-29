a4ab62f80cae9d96ce65eddba442a65604c658f8
peter <peter@localhost>
2013-01-29 15:03:38 +0800

    don't show profile if user's profile is empty

---
+++ grubcat/eo/decorators.py
@@ -0,0 +1,61 @@
+import django
+from django.contrib.auth.decorators import user_passes_test
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse
+from django.utils import simplejson
+
+try:
+    from functools import wraps
+except ImportError:
+    from django.utils.functional import wraps  # Python 2.4 fallback.
+
+from django.contrib.auth import REDIRECT_FIELD_NAME
+
+def ajax_login_required(view_func):
+    @wraps(view_func)
+    def wrap(request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return view_func(request, *args, **kwargs)
+        json = simplejson.dumps({ 'not_authenticated': True })
+        return HttpResponse(json, mimetype='application/json')
+    return wrap
+
+def restaurant_login_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None):
+    """
+    Decorator for views that checks that the user is logged in, redirecting
+    to the log-in page if necessary.
+    """
+    if not login_url:
+        login_url = django.conf.settings.RESTAURANT_LOGIN_URL
+    actual_decorator = user_passes_test(
+        test_restaurant_user,
+        login_url=login_url,
+        redirect_field_name=redirect_field_name
+    )
+    if function:
+        return actual_decorator(function)
+    return actual_decorator
+
+def active_login_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None):
+    """
+    Decorator for views that checks that the user is logged in, redirecting
+    to the log-in page if necessary.
+    """
+    actual_decorator = user_passes_test(
+        lambda u: u.is_authenticated() and u.is_active,
+        login_url=login_url,
+        redirect_field_name=redirect_field_name
+    )
+    if function:
+        return actual_decorator(function)
+    return actual_decorator
+
+
+def test_restaurant_user(user):
+    if user.is_authenticated():
+        try:
+            restaurant = user.restaurant
+            return True
+        except ObjectDoesNotExist: pass
+    return False
+
---
+++ grubcat/eo/exceptions.py
@@ -0,0 +1,22 @@
+#coding=utf-8
+from django.conf import settings
+from django.template.response import TemplateResponse
+from django.views.generic.simple import direct_to_template
+from raven.contrib.django.models import sentry_exception_handler
+
+class BusinessException(Exception):
+    message = u"对不起，您的操作有误！"
+
+class NoAvailableSeatsError(BusinessException):
+    message = u"对不起，饭局人数已满，您可以加入其他感兴趣的饭局！"
+
+class AlreadyJoinedError(BusinessException):
+    message = u"对不起，您已经加入了这个饭局，您可以加入其他感兴趣的饭局！"
+class NoRightException(BusinessException):
+    message = u"对不起，您没有权限执行此操作！"
+
+class ProcessExceptionMiddleware(object):
+    def process_exception(self, request, exception):
+        if not settings.DEBUG:
+            sentry_exception_handler(request=request)
+            return TemplateResponse(request, "500.html", {'exception': exception})
