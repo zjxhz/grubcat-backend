1729fcba383e28237c85796b2f22d0099d40d337
peter <peter@localhost>
2013-01-30 18:32:38 +0800

    highlit selected tags in edit profile page

--- grubcat/eo/assets.py
+++ grubcat/eo/assets.py
@@ -46,7 +46,7 @@ user_css = Bundle(
 )
 
 profile_css = Bundle(
-    'css/jquery.Jcrop.css', 'css/autoSuggest.css', common_css, 'less/profile.less', responsive_css,
+    'css/jquery.Jcrop.css', 'less/autoSuggest.less', common_css, 'less/profile.less', responsive_css,
     filters='less,cssmin', output='gen/profile.%(version)s.css'
 )
 
