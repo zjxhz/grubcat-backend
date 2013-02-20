49f7d9ab5b059f6353f063085ff79c7b035bc5a7
peter <peter@localhost>
2013-02-20 22:15:02 +0800

    feature: alipay

--- grubcat/eo/exceptions.py
+++ grubcat/eo/exceptions.py
@@ -5,28 +5,32 @@ from raven.contrib.django.models import sentry_exception_handler
 
 
 class BusinessException(Exception):
-    message = u"对不起，您的操作有误！"
+    pass
 
 
 class NoAvailableSeatsError(BusinessException):
-    message = u"对不起，饭局人数已满，您可以加入其他感兴趣的饭局！"
+    pass
 
 
 class AlreadyJoinedError(BusinessException):
-    message = u"对不起，您已经加入了这个饭局，您可以加入其他感兴趣的饭局！"
+    pass
 
 
 class NoRightException(BusinessException):
-    message = u"对不起，您没有权限执行此操作！"
+
+    def __init__(self, message=u"对不起，您没有权限执行此操作！"):
+        self.message = message
 
 
 class AliapyBackVerifyFailedError(BusinessException):
-    message = u"对不起，出错了，请您查看订单是否支付成功！"
+    def __init__(self, message=u"对不起，出错了，请您查看订单是否支付成功！"):
+        self.message = message
 
 
 #担保交易换成及时支付后，会设置支付有效时间，不会出现这种异常
 class PayOverTimeError(BusinessException):
-    message = u'对不起，您支付已经超时，请联系饭聚网退款！'
+    def __init__(self, message=u"对不起，您支付已经超时，请联系饭聚网退款！"):
+        self.message = message
 
 
 class ProcessExceptionMiddleware(object):
