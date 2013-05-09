4d2218e2c2f4b5299f918c3001bfadcc6d3efd33
peter <peter@localhost>
2013-05-09 16:44:59 +0800

    add sentry server for test server

--- grubcat/settings_test.py
+++ grubcat/settings_test.py
@@ -20,75 +20,8 @@ WEIBO_APP_SECERT = "edc858db52e5c2bc803010a81b183c5d"
 WEIBO_REDIRECT_URL = "http://www.ifunjoin.com/login/weibo/"
 
 RAVEN_CONFIG = {
-    'register_signals': False,
-    # 'dsn': 'http://e113732a1ddc462f9183b1038e4af184:58a864b1292a40179f900fffc8d02b9e@www.fanjoin.com:9000/2',
+    'register_signals': True,
+    'dsn': 'http://66a385f77d6a4fa3ab75db245d66695a:0d7f487d06a74dbaa634d894f77703d6@www.ifunjoin.com:9000/2',
     }
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'root': {
-        'level': 'DEBUG',
-        'handlers': ['default'],
-    },
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
-        },
-    },
-    'handlers': {
-        'default': {
-            'level': 'DEBUG',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': os.path.join(LOGGING_ROOT, 'all.log'),
-            'maxBytes': 1024 * 1024 * 2, # 5 MB
-            'backupCount': 7,
-            'formatter': 'verbose',
-        },
-        'pay': {
-            'level': 'DEBUG',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': os.path.join(LOGGING_ROOT, 'pay.log'),
-            'maxBytes': 1024 * 1024 * 2, # 5 MB
-            'backupCount': 7,
-            'formatter': 'verbose',
-        },
-        'sentry': {
-            'level': 'ERROR',
-            'class': 'raven.contrib.django.handlers.SentryHandler',
-        },
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        }
-    },
-    'loggers': {
-        'django.db.backends': {
-            'level': 'INFO',
-            'handlers': ['default'],
-            'propagate': False,
-        },
-        'raven': {
-            'level': 'ERROR',
-            'handlers': ['default'],
-            'propagate': False,
-        },
-        'sentry.errors': {
-            'level': 'DEBUG',
-            'handlers': ['default'],
-            'propagate': False,
-        },
-        'api': {
-            'handlers': ['default'],
-            'level': 'DEBUG',
-            'propagate': False
-        },
-        'pay': {
-            'handlers': ['pay'],
-            'level': 'DEBUG',
-            'propagate': False
-        }
-    }
-}
 
