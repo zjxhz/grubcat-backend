3bf46db943042937f5eadca0b56a621f67318ce6
peter <peter@localhost>
2013-05-25 22:50:23 +0800

    set domain name xx.com

--- grubcat/settings_test.py
+++ grubcat/settings_test.py
@@ -15,14 +15,12 @@ ALIPAY_BACK_DOMAIN = 'http://ifunjoin.com/'
 ORDER_PREFIX = 'det1'
 PAY_DEBUG = False
 
-SESSION_COOKIE_DOMAIN = 'ifunjoin.com'
-
 #WEIBO_APP_KEY = "4071331500"
 #WEIBO_APP_SECERT = "5cf4910b217617cee72b2889a8e394eb"
 
 WEIBO_APP_KEY = "1086545555"
 WEIBO_APP_SECERT = "edc858db52e5c2bc803010a81b183c5d"
-WEIBO_REDIRECT_URL = "http://www.ifunjoin.com/login/weibo/"
+WEIBO_REDIRECT_URL = "http://ifunjoin.com/login/weibo/"
 
 RAVEN_CONFIG = {
     'register_signals': True,
